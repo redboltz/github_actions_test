64886b7e5570bf74c155dc95973492a9aa25cada
Takatoshi Kondo <redboltz@gmail.com>
2020-12-12 17:14:51 +0900

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -4,4 +4,7 @@ int main() {
 #if defined(__GLIBCXX__)
     std::cout << __GLIBCXX__ << std::endl;
 #endif
+    std::cout << "cout" << std::endl;
+    std::cerr << "cerr" << std::endl;
+    std::clog << "clog" << std::endl;
 }
